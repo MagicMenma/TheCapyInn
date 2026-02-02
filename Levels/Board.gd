cbe2e622ad392fc56efe44366674eccef8fa1dcc
MagicMenma <1479800445@qq.com>
2026-02-02 17:41:53 -0500

    0.24 Noren Update / Fix
    w

--- Levels/Board.gd
+++ Levels/Board.gd
@@ -21,7 +21,7 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _check_and_toggle_noren():
+func _check_is_view_size_mobile() -> bool:
 	# 获取视口的实际尺寸
 	var view_size = get_viewport().get_visible_rect().size
 	# 计算纵横比 (高 / 宽)
@@ -29,25 +29,33 @@ func _check_and_toggle_noren():
 	var aspect_ratio = view_size.y / view_size.x
 	# 如果比值大于 1.3，通常意味着是竖屏模式（手机或窄窗口）
 	if aspect_ratio > 1.3:
-		noren.visible = true
-		print("检测到移动端比例，暖帘已开启")
-		noren.visible = true
-		noren.modulate.a = 0 # 初始透明
-		noren.position.y = -50 # 初始位置稍高
-		var tween = create_tween().set_parallel(true)
-		tween.tween_property(noren, "modulate:a", 1.0, 1)
-		tween.tween_property(noren, "position:y", 0.0, 2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+		return true
 	else:
-		noren.visible = false
-		print("检测到宽屏/网页比例，暖帘已隐藏")
+		return false
 
+func _check_and_toggle_noren():
+	noren.visible = true
+	print("检测到移动端比例，暖帘已开启")
+	noren.visible = true
+	noren.modulate.a = 0 # 初始透明
+	noren.position.y = -80 # 初始位置稍高
+	var tween = create_tween().set_parallel(true)
+	tween.tween_property(noren, "modulate:a", 1.0, 1)
+	tween.tween_property(noren, "position:y", 0.0, 2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
 
-
+func _close_noren():
+	if !_check_is_view_size_mobile():
+		noren.modulate.a = 1 # 初始透明
+		noren.position.y = 0 # 初始位置稍高
+		var tween = create_tween().set_parallel(true)
+		tween.tween_property(noren, "position:y", -150, 2).set_trans(Tween.TRANS_BACK).set_ease(Tween.EASE_OUT)
+		tween.tween_property(noren, "modulate:a", 0, 2)
 
 func _on_game_over_area_overflow_occurred() -> void:
 	# 显示 UI + 更新 GameOverInterface
 	game_over_ui.visible = true
 	game_over_ui.update_interface()
+	_close_noren()
 	
 # 处理分数区域
 # 这是一个内部处理函数，专门负责更新文字
